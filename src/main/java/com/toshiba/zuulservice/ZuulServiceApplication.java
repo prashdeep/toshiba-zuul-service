3eadce8dec465babbdeac4fe5630ab6d7e5c91b8
prashdeep <pradeep.kumar44@gmail.com>
2019-11-27 10:29:24 +0530

    committing the pre filter implementation

--- src/main/java/com/toshiba/zuulservice/ZuulServiceApplication.java
+++ src/main/java/com/toshiba/zuulservice/ZuulServiceApplication.java
@@ -2,12 +2,14 @@ package com.toshiba.zuulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZuulServiceApplication.class, args);
     }
